337808d05890a95dcb84601d07f24483d83c4edf
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2022-11-02 21:04:33 +0700

    chore: [AR] add file dart saveRandom

---
+++ lib/Main/PageSave/KategoriSave/saveRandom.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+class SaveRandom extends StatefulWidget {
+  const SaveRandom({Key? key}) : super(key: key);
+
+  @override
+  State<SaveRandom> createState() => _SaveRandomState();
+}
+
+class _SaveRandomState extends State<SaveRandom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
