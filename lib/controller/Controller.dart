3044e0eb419f72b2f87b2ac5c062b939cf0b3d87
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-01-19 18:56:04 +0700

    fix: [AR] firebase push notif

---
+++ lib/controller/Controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:thehorizonapps/Model/OnThisDayModel.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DataController extends GetxController{
+  OnThisDayModel? onThisDayModel;
+  var isDataLoading = false.obs;
+  //get mount
+  String mount = DateTime.now().month.toString();
+  //get day
+  String day = DateTime.now().day.toString().padLeft(2, '0');
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    getApi(mount, day);
+  }
+
+  @override
+  Future<void> onReady() async {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+
+  getApi(mount, day) async {
+    isDataLoading(true);
+    final responseOtd = await http.get(Uri.parse('https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/${mount}/${day}'));
+    print("Response status: ${responseOtd.statusCode}");
+    onThisDayModel = OnThisDayModel.fromJson(jsonDecode(responseOtd.body.toString()));
+    print("Response body: ${onThisDayModel?.events![0].text}");
+    isDataLoading(false);
+  }
+}
