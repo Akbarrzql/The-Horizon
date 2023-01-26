e9d2dc09c9e324f962358c1f84e84df596e7f6a1
AZ <wira123widodo@gmail.com>
2023-01-27 02:54:36 +0700

    style[AW]: create responsive in file main_quiz

--- lib/Main/PageSave/PageSave.dart
+++ lib/Main/PageSave/PageSave.dart
@@ -54,14 +54,11 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 35, left: 20),
                                       child: Text('Sejarah Hari ini', style: GoogleFonts.poppins(fontSize: 19, fontWeight: FontWeight.w500, color: Colors.white),),
                                     ),
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 5, left: 20),
                                       child: Text('Tersimpan dari Sejarah Hari ini', style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.normal, color: Colors.white),),
                                     ),
-                                    // SizedBox(width: 50,),
                                   ],
                                 ),
                               ),
@@ -72,9 +69,6 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                 children: [
                                   Container(
                                     constraints: BoxConstraints(maxWidth: 130),
-                                    //image with border radius
-                                    // padding: const EdgeInsets.only(right: 10),
-                                    // margin: const EdgeInsets.only(left: 45),
                                     child: ClipRRect(
                                       borderRadius: BorderRadius.only(
                                         topLeft: Radius.circular(40),
@@ -123,14 +117,11 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 35, left: 20),
                                       child: Text('Bacaan Teratas', style: GoogleFonts.poppins(fontSize: 19, fontWeight: FontWeight.w500, color: Colors.white),),
                                     ),
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 5, left: 20),
                                       child: Text('Tersimpan dari Bacaan Teratas', style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.normal, color: Colors.white),),
                                     ),
-                                    // SizedBox(width: 50,),
                                   ],
                                 ),
                               ),
@@ -141,9 +132,6 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                 children: [
                                   Container(
                                     constraints: BoxConstraints(maxWidth: 130),
-                                    //image with border radius
-                                    // padding: const EdgeInsets.only(right: 10),
-                                    // margin: const EdgeInsets.only(left: 45),
                                     child: ClipRRect(
                                       borderRadius: BorderRadius.only(
                                         topLeft: Radius.circular(40),
@@ -192,14 +180,11 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 35, left: 20),
                                       child: Text('Artikel Acak', style: GoogleFonts.poppins(fontSize: 19, fontWeight: FontWeight.w500, color: Colors.white),),
                                     ),
                                     Container(
-                                      // margin: const EdgeInsets.only(top: 5, left: 20),
                                       child: Text('Tersimpan dari Artikel Acak', style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.normal, color: Colors.white),),
                                     ),
-                                    // SizedBox(width: 50,),
                                   ],
                                 ),
                               ),
@@ -210,9 +195,6 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                                 children: [
                                   Container(
                                     constraints: BoxConstraints(maxWidth: 130),
-                                    //image with border radius
-                                    // padding: const EdgeInsets.only(right: 10),
-                                    // margin: const EdgeInsets.only(left: 45),
                                     child: ClipRRect(
                                       borderRadius: BorderRadius.only(
                                         topLeft: Radius.circular(40),
@@ -230,111 +212,6 @@ class _PageSaveKategoriState extends State<PageSaveKategori> {
                       ),
                     ),
                   ),
-                  // Container(
-                  //   margin: const EdgeInsets.only(top: 10, right: 5, left: 5),
-                  //   height: 130,
-                  //   child: InkWell(
-                  //     onTap: (){
-                  //       Navigator.push(context, MaterialPageRoute(builder: (context) => Pagesave()));
-                  //     },
-                  //     child: Card(
-                  //       shape: const RoundedRectangleBorder(
-                  //         borderRadius: BorderRadius.only(
-                  //           topLeft: Radius.circular(15),
-                  //           topRight: Radius.circular(5),
-                  //           bottomLeft: Radius.circular(40),
-                  //           bottomRight: Radius.circular(5),
-                  //         ),
-                  //       ),
-                  //       color: Color(0xff5FD068),
-                  //       child: Row(
-                  //         crossAxisAlignment: CrossAxisAlignment.start,
-                  //         children: [
-                  //           Column(
-                  //             crossAxisAlignment: CrossAxisAlignment.start,
-                  //             children: [
-                  //               Container(
-                  //                 margin: const EdgeInsets.only(top: 35, left: 20),
-                  //                 child: Text('Bacaan teratas', style: GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w500, color: Colors.white),),
-                  //               ),
-                  //               Container(
-                  //                 margin: const EdgeInsets.only(top: 5, left: 20),
-                  //                 child:  Text('Tersimpan dari bacaan teratas',style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.normal, color: Colors.white),),
-                  //               ),
-                  //               SizedBox(width: 50,),
-                  //             ],
-                  //           ),
-                  //           Container(
-                  //             //image with border radius
-                  //             padding: const EdgeInsets.only(right: 5),
-                  //             margin: const EdgeInsets.only(top: 10, left: 30),
-                  //             child: ClipRRect(
-                  //               borderRadius: BorderRadius.only(
-                  //                 topLeft: Radius.circular(40),
-                  //                 topRight: Radius.circular(5),
-                  //                 bottomLeft: Radius.circular(15),
-                  //                 bottomRight: Radius.circular(5),
-                  //               ),
-                  //               child: Image.asset('assets/tersimpanteratas.jpg', width: 100, height: 100, fit: BoxFit.cover,),),
-                  //           ),
-                  //         ],
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  // Container(
-                  //   margin: const EdgeInsets.only(top: 10, right: 5, left: 5),
-                  //   height: 130,
-                  //   width: 350,
-                  //   child: InkWell(
-                  //     onTap: (){
-                  //       Navigator.push(context, MaterialPageRoute(builder: (context) => SaveRandom()));
-                  //     },
-                  //     child: Card(
-                  //       shape: const RoundedRectangleBorder(
-                  //         borderRadius: BorderRadius.only(
-                  //           topLeft: Radius.circular(15),
-                  //           topRight: Radius.circular(5),
-                  //           bottomLeft: Radius.circular(40),
-                  //           bottomRight: Radius.circular(5),
-                  //         ),
-                  //       ),
-                  //       color: Color(0xff5FD068),
-                  //       child: Row(
-                  //         crossAxisAlignment: CrossAxisAlignment.start,
-                  //         children: [
-                  //           Column(
-                  //             crossAxisAlignment: CrossAxisAlignment.start,
-                  //             children: [
-                  //               Container(
-                  //                 margin: const EdgeInsets.only(top: 35, left: 20),
-                  //                 child: Text('Artikel Acak', style: GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w500, color: Colors.white),),
-                  //               ),
-                  //               Container(
-                  //                 margin: const EdgeInsets.only(top: 5, left: 20),
-                  //                 child:  Text('Tersimpan dari Artikel Acak', style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.normal, color: Colors.white),),
-                  //               ),
-                  //               SizedBox(width: 50,),
-                  //             ],
-                  //           ),
-                  //           Container(
-                  //             //image with border radius
-                  //             padding: const EdgeInsets.only(right: 5),
-                  //             margin: const EdgeInsets.only(top: 10, left: 52),
-                  //             child: ClipRRect(
-                  //               borderRadius: BorderRadius.only(
-                  //                 topLeft: Radius.circular(40),
-                  //                 topRight: Radius.circular(5),
-                  //                 bottomLeft: Radius.circular(15),
-                  //                 bottomRight: Radius.circular(5),
-                  //               ),
-                  //               child: Image.asset('assets/artikelacak.jpg', width: 100, height: 100, fit: BoxFit.cover,),),
-                  //           ),
-                  //         ],
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               ),
             )
